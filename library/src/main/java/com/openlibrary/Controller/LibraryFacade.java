0e21a03c71a63653e17bffa045be9385c44d7d05
strashacc <sayantrashacc@gmail.com>
2024-11-06 23:09:31 +0500

    Create a high level interface for interactions between Controller and View

---
+++ library/src/main/java/com/openlibrary/Controller/LibraryFacade.java
@@ -0,0 +1,24 @@
+package com.openlibrary.Controller;
+
+import java.util.List;
+
+public class LibraryFacade {
+    
+    public LibraryFacade(){
+        
+    }
+
+    public List<Book> getBooks(){
+        //TODO: Generate concrete implementation
+    }
+
+    public void addBook(String type, String title, String author, String imgLink){
+        //TODO: Generate concrete implementation
+    }
+    public void addExternalBook(ExternalBook externalBook){
+        //TODO: Generate concrete implementation
+    }
+    public void removeBook(int id){
+        //TODO: Generate concrete implementation
+    }
+}
